7013249154d303f4996a11c59b1625bd513ae46b
dariogabrielnunez <132526136+dariogabrielnunez@users.noreply.github.com>
2023-06-15 01:55:33 -0300

    Add files via upload

---
+++ Punto 5/CalculadoraPreciosRestaurante/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace CalculadoraPreciosRestaurante
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double precioPancho = 2.0;
+            double precioPapasFritas = 1.0;
+            double precioGaseosa = 0.85;
+
+            Console.WriteLine("Cantidad de Panchos:");
+            int cantidadPanchos = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Cantidad de porciones de Papas Fritas:");
+            int cantidadPapasFritas = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Cantidad de Gaseosas:");
+            int cantidadGaseosas = Convert.ToInt32(Console.ReadLine());
+
+            double totalPanchos = precioPancho * cantidadPanchos;
+            double totalPapasFritas = precioPapasFritas * cantidadPapasFritas;
+            double totalGaseosas = precioGaseosa * cantidadGaseosas;
+            double subtotal = totalPanchos + totalPapasFritas + totalGaseosas;
+            double propina = subtotal * 0.10;
+            double total = subtotal + propina;
+
+            Console.WriteLine("\n--------------- COMPROBANTE ------------------");
+            Console.WriteLine("Producto \tPrecioUnitario \tCant \tTotal");
+            Console.WriteLine("Pancho \t\t${0} \t\t{1} \t${2}", precioPancho, cantidadPanchos, totalPanchos);
+            Console.WriteLine("Papas Fritas \t${0} \t\t{1} \t${2}", precioPapasFritas, cantidadPapasFritas, totalPapasFritas);
+            Console.WriteLine("Gaseosas \t${0} \t\t{1} \t${2}", precioGaseosa, cantidadGaseosas, totalGaseosas);
+            Console.WriteLine("-----------------------------------------------");
+            Console.WriteLine("Propina(10%) \t\t\t\t${0}", propina);
+            Console.WriteLine("-----------------------------------------------");
+            Console.WriteLine("Total \t\t\t\t\t${0}", total);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Punto 6/HostGenerico/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace HostGenerico
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var nuevoHost = CreateHostBuilder(args).Build();
+            var logger = nuevoHost.Services.GetService<ILogger<Program>>();    
+            logger.LogInformation("Work in Progress...");
+            nuevoHost.Run();
+            Console.ReadLine();
+
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) => Host.CreateDefaultBuilder(args).ConfigureLogging(logging =>
+        {
+            logging.ClearProviders();
+            logging.AddConsole();
+        });
+
+    }
+}
